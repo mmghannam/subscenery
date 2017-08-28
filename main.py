b395d9c747d40373ba4c5c4aa41627543b96a35c
mmghannam <mohammad.mohsen@hotmail.com>
2017-08-28 19:39:56 +0200

    packaged scrapper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from scrapper import SubSceneScrapper
 from pprint import pprint
+
+from subscenery.scrapper import SubSceneScrapper
 
 movie_name = 'August Rush'
 scrapper = SubSceneScrapper(movie_name)
